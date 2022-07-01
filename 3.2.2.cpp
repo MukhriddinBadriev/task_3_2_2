8a2978ce2aa29214e4f7c7b4d5480c9bd001835b
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-07-02 00:48:51 +0300

    Bank account

---
+++ 3.2.2.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+struct Bank
+{
+    int account;
+    string name;
+    double money;
+};
+void bank(Bank &u,double new_money) {
+    cout << "Введите номер счёта: ";
+    cin >> u.account;
+    cout << "Введите имя владельца: ";
+    cin >> u.name;
+    cout << "Введите баланс: ";
+    cin >> u.money;
+    cout << "Введите новый баланс: ";
+    cin >> new_money;
+    cout <<"Ваш счёт: " << u.name << ", " << u.account << ", " << new_money<<endl;
+}
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    Bank man;
+    double new_money = 0;  
+    bank(man, new_money);
+    return 0;
+}
