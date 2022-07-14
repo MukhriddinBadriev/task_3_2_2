d7886dd33071ef1d97965a2a437ef8c22c3825b5
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-07-14 16:14:37 +0300

    Счета

--- 3.2.2.cpp
+++ 3.2.2.cpp
@@ -6,23 +6,23 @@ struct Bank
     string name;
     double money=0;
 };
-void bank(Bank &u,double new_money) {
-    
-    cout << "Введите новый баланс: ";
-    cin >> new_money;
+void bank(Bank &u,double new_money) {   
     u.money = new_money;
 }
 int main()
 {
     setlocale(LC_ALL, "rus");
-    Bank man;  
+    Bank man; 
+    double new_money;
     cout << "Введите номер счёта: ";
     cin >> man.account;
     cout << "Введите имя владельца: ";
     cin >> man.name;
     cout << "Введите баланс: ";
     cin >> man.money;
-    bank(man,1);
+    cout << "Введите новый баланс: ";
+    cin >> new_money;
+    bank(man, new_money);
     cout << "Ваш счёт: " << man.name << ", " << man.account << ", " << man.money << endl;
     return 0;
 }
